114080fc14bea7954ebed455ce765dd06f810565
Esubalew <122857025+Maesfen@users.noreply.github.com>
2023-02-17 21:55:59 +0300

    a program that prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,27 +7,31 @@
 int main(void)
 
 {
-	int number_left;
-	int number_right;
+int number_left;
+int number_right;
 
-	for (number_left = 48; number_right <= 78; number_left++)
-	{
-		for (number_right = number_left + 1 ; number_right <= 78; number_right++)
-		{
-			putchar(number_left);
-			putchar (number_right);
+for (number_left = 48; number_right <= 78; number_left++)
+{
+for (number_right = number_left + 1 ; number_right <= 78; number_right++)
+{
 
-			if ((number_left == 56) && (number_right == 78))
-			{
-				break;
-			}
+putchar(number_left);
+putchar (number_right);
 
-			putchar(',');
-			putchar (' ');
-		}
-	}
+if ((number_left == 56) && (number_right == 78))
+{
+break;
+}
 
-	putchar('\n');
+putchar(',');
+putchar (' ');
 
-	return (0);
+}
+
+}
+
+putchar('\n');
+
+return (0);
+
 }
