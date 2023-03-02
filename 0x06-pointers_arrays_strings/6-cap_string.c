0e2d40232dd0e2847b0233717887e36002d39694
Maesfen <esubalewfentahun38@gmail.com>
2023-03-02 13:42:01 -0800

    commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,17 +2,20 @@
 #include <string.h>
 /**
  * *cap_string - Description
+ *  @c: inputted string
  *
- * Return: 0
+ * Return: returns a converted string
  */
 char *cap_string(char *c)
 {
 	int i;
 
-	for (i = 0;  c[i] != '\0'; i++ )
+	for (i = 0;  c[i] != '\0'; i++)
 	{
-		if (c[i] > 96 && c[i] < 123 && (c[i - 1] == ' ' || c[i - 1] == '\n' || c[i - 1] == '\t' || c[i - 1] == ','
-					|| c[i - 1] == ';' || c[i - 1] == '.' || c[i - 1] == '!' || c[i - 1] == '?' || c[i - 1] == '"' || c[i - 1] == '('
+		if (c[i] > 96 && c[i] < 123 && (c[i - 1] == ' '
+				       	|| c[i - 1] == '\n' || c[i - 1] == '\t' || c[i - 1] == ','
+					|| c[i - 1] == ';' || c[i - 1] == '.' 
+					|| c[i - 1] == '!' || c[i - 1] == '?' || c[i - 1] == '"' || c[i - 1] == '('
 					|| c[i - 1] == ')' || c[i - 1] == '{' || c[i - 1] == '}' || i == 0))
 			c[i] = c[i] - 32;
 	}
