0c5c7ef2ca6f8d69e885da4e43b4add8a5f7f8a5
Esubalew <122857025+Maesfen@users.noreply.github.com>
2023-03-03 01:35:13 +0300

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,35 +1,29 @@
 #include "main.h"
 #include <string.h>
 /**
- * *_strcat - Description
- * @src: inputted string
- * @dest: inputted string
- *
+ * main - description
  * Return: 0
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+        int i, j;
 
-	for (i = 0 ; i < 1000 ; i++)
-	{
-		if ((*(dest + i)) == '\0')
-			break;
-	}
-	j = 0;
+        i = 0;
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
 
-	while ((*(src + j)) != '\0')
-	{
-		(*(dest + i)) = (*(src + j));
-		j++;
-		i++;
+        j = 0;
+        while (src[j] != '\0')
+        {
+                dest[i] = src[j];
+                j++;
+                i++;
+        }
 
-		if ((*(src + j)) == '\0')
-		{
-			(*(dest + i)) = (*(src + j));
-			break;
-		}
-	}
+        dest[i] = '\0';
 
-	return (dest);
+        return (dest);
 }
