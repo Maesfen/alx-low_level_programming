9d9222d1e2085ddc3a85293c5992316dbd30996d
Esubalew <122857025+Maesfen@users.noreply.github.com>
2023-03-02 18:53:57 +0300

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,9 +7,9 @@
  */
 void print_alphabet(void)
 {
-	char alphabet = 'a';
+	char alphabet;
 
-	while (alphabet <= 'z')
+	for (alphabet = 'a'; alphabet  <= 'z'; alphabet++)
 	{
 		_putchar(alphabet);
 		alphabet++;
