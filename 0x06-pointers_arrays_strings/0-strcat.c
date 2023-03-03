2274def965c9e17ab0a2dddff7b38fce8e1a94c1
Maesfen <esubalewfentahun38@gmail.com>
2023-03-03 05:24:42 -0800

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,56 +1,35 @@
 #include "main.h"
 #include <string.h>
-/**
-<<<<<<< HEAD
- * main - description
-=======
- * _strcat - description
- * @src: A pointer to a character that will be changed
- * @dest: A pointer to a character that will also be changed
- *
->>>>>>> 28452a65173499cfb83604cd25da3b0d013ceeb5
- * Return: 0
- */
 
+/**
+ * *_strcat - This function concatenates two strings
+ * @src: inputted string
+ * @dest: inputted string
+ *
+ * Return: it returns a concattenated string
+ */
 char *_strcat(char *dest, char *src)
 {
-        int i, j;
+	int i, j;
 
-<<<<<<< HEAD
-	i = 0;
-	while (dest[i] != '\0')
+	for (i = 0 ; i < 1000 ; i++)
 	{
-		i++;
+		if ((*(dest + i)) == '\0')
+			break;
 	}
-
 	j = 0;
-	while (src[j] != '\0')
+	while ((*(src + j)) != '\0')
 	{
-		dest[i] = src[j];
+		(*(dest + i)) = (*(src + j));
 		j++;
 		i++;
+
+		if ((*(src + j)) == '\0')
+		{
+			(*(dest + i)) = (*(src + j));
+			break;
+		}
 	}
-
-	dest[i] = '\0';
-
 	return (dest);
-=======
-        i = 0;
-        while (dest[i] != '\0')
-        {
-                i++;
-        }
 
-        j = 0;
-        while (src[j] != '\0')
-        {
-                dest[i] = src[j];
-                j++;
-                i++;
-        }
-
-        dest[i] = '\0';
-
-        return (dest);
->>>>>>> 28452a65173499cfb83604cd25da3b0d013ceeb5
 }
