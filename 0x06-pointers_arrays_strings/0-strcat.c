f9c3d809a50cdca7d02318f86179d925c77ea0bb
Maesfen <esubalewfentahun38@gmail.com>
2023-03-03 05:33:41 -0800

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,6 @@
  * Return: it returns a concattenated string
  */
 char *_strcat(char *dest, char *src)
-<<<<<<< HEAD
 {
 	int i, j;
 
@@ -25,34 +24,12 @@ char *_strcat(char *dest, char *src)
 		j++;
 		i++;
 
-=======
-		{
-		int i, j;
-
-		for (i = 0 ; i < 1000 ; i++)
-		{
-		if ((*(dest + i)) == '\0')
-		break;
-		}
-		j = 0;
-		while ((*(src + j)) != '\0')
-		{
-			(*(dest + i)) = (*(src + j));
-			j++;
-			i++;
->>>>>>> f05c8d0417976c02679a2a0517b9eb86616bb7b7
 		if ((*(src + j)) == '\0')
 		{
 			(*(dest + i)) = (*(src + j));
 			break;
 		}
-<<<<<<< HEAD
 	}
 	return (dest);
 
 }
-=======
-		}
-		return (dest);
-		}
->>>>>>> f05c8d0417976c02679a2a0517b9eb86616bb7b7
