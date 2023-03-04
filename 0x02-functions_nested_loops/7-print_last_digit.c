26815b9eddcb257fc9c89faee769b38049fdd487
Maesfen <esubalewfentahun38@gmail.com>
2023-03-04 04:25:10 -0800

    updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,18 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * print_last_digit - prints the last digit of a number
- * @n: n is an integer
- * Description: prints the last digit of a number
- * Return: integer
+ * print_last_digit - prints the last digit of a number.
+ * @n: random intiger
+ * Description: Write a function that prints the last digit of a number
+ *
+ * Return: last digit
  */
 int print_last_digit(int n)
 {
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
 	int last = n % 10;
 
 	if (n < 0)
