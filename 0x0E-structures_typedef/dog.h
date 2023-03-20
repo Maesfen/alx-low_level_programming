d9956ddb2d9dd9d08627c72e3f6c3bff8f83469a
maesfen <esubalewfentahun38@gmail.com>
2023-03-20 12:36:04 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
-#ifndef dog_h
-#define dog_h
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct dog - initializes a structure
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
+ */
+
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 struct dog
 {
@@ -12,5 +18,16 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
+
