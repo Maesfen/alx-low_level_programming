d981a71e9fb8cd3c4725f96dbac4ad7ffd1de416
maesfen <esubalewfentahun38@gmail.com>
2023-03-20 11:47:48 -0700

    commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+nclude <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: it returns 0 when exit
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
